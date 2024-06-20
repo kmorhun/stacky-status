2df3cae0b76d075c036dd8b6d7f52c9895d41e0e
Kateryna Morhun <kmorhun@gmail.com>
2024-06-20 13:28:16 -0400

    created status page

---
+++ src/lib/StatusMarker.svelte
@@ -0,0 +1,65 @@
+<div>
+    <h3>{props.systemName}</h3>
+    <p><span class="cardLabel">Port:</span> {props.systemPort == 0 ? "Null" : props.systemPort}</p>
+    <p><span class="cardLabel">Mode:</span> {props.mode}</p>
+    <p><span class="cardLabel">URL:</span> <a href=props.url>{props.url}</a></p>
+    <p><span class="cardLabel">Status:</span> <span class="status" style="color: {statusColor}">{systemStatus}</span></p>
+</div>
+
+<style>
+    .status {
+        font-weight: bold;
+    }
+
+    .cardLabel {
+        font-weight: bold;
+    }
+
+    div {
+        border: 1px solid black;
+        padding: 25px;
+        margin: 10px;
+        border-radius: 10px;
+    }
+    
+</style>
+
+<script>
+    import axios from 'axios';
+    export let props =  {
+        systemName: "Test System",
+        systemPort: 3000,
+        mode: "development", // or "production"
+        url: "http://beta.stacky.social:3000/ping"
+    };
+
+    let systemStatus;
+
+    let success = null;
+
+    // generate a new random number every 5 seconds
+    $: {    
+        setInterval(() => {
+            axios.get(props.url)
+                .then((res) => {
+                    if (res.status == 200) {
+                        console.log(props.systemName + " is connected :)")
+                        success = true;
+                    } else {
+                        console.log(res.status)
+                        console.log(props.systemName + " is not connected :(")
+                        success = false;
+                    }
+                })
+                .catch((res) => {
+                    console.log(res)
+                    success = false;
+                });
+        }, 5000);
+    }
+
+    $: systemStatus = (success == null) ? "Loading..." : (success ? "Online :)" : "Offline :(");
+    $: statusColor = (success == null) ? "Blue" : (success ? "Green" : "Red");;
+
+  
+</script>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,51 @@
+<div class="logo">
+    <!-- image of logo -->
+    <img src="stacky.png" alt="Stacky Logo">
+</div>
+
+<div class="statusList">
+    {#each allProps as props}
+        <StatusMarker props={props}></StatusMarker>
+    {/each}
+</div>
+
+<style>
+    img {
+        margin-top: 5vw;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 300px;
+    }
+    .statusList {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        font-family: 'Avenir', sans-serif;
+        font-stretch: ultra-wide;
+    }
+</style>
+
+<script>
+    import StatusMarker from '$lib/StatusMarker.svelte';
+
+    let allProps = [
+        {
+            systemName: "Mastodon",
+            systemPort: 3000,
+            mode: "Production",
+            url: "https://beta.stacky.social/api/v1/timelines/public"
+        },
+        {
+            systemName: "Curate",
+            systemPort: 3002,
+            mode: "Production",
+            url: "https://beta.stacky.social:3002/ping"
+        }
+        
+    ]
+
+    allProps.sort((a, b) => a.systemName.localeCompare(b.systemName));
+
+</script>
